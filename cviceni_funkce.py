deed40f399cd0722576429374a968a371e0db27a
MarketKapi <marketa.kapickova@gmail.com>
2025-05-02 18:39:54 +0200

    Unnecessary file deleted.

--- cviceni_funkce.py
+++
@@ -1,9 +0,0 @@
-vstupni_text = "Ahoj všem, tady Engeto"
-def zdvojnasob_znaky(text):
-  zdvojene = list()
-  for znak in text:
-    znak += znak
-    zdvojene.append(znak)
-  return zdvojene
-vysledek = zdvojnasob_znaky(vstupni_text)
-print("Zdvojene znaky:","".join(vysledek))
